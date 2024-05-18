1e36341806821678fdf04bd76c6db720804bc5bc
Imed Bousakhria <i_bousakhria@estin.dz>
2024-05-18 03:08:44 +0100

    init chores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+    },
   },
   plugins: [],
-}
-
+};

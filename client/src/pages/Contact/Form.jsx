2efecac8367680991967cb77a2e5499e3b605837
whoamif <adjissifatimaamina@gmail.com>
2024-06-19 21:36:22 +0100

    up + add + link: update and add the contact us backend logic
    in this update i add some parameters  in the data body and aslo i link btw the form and the endpoint

--- client/src/pages/Contact/Form.jsx
+++ client/src/pages/Contact/Form.jsx
@@ -1,155 +1,176 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 const ContactForm = () => {
-   
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    inquiryType: 'Produit',
-    category: 'Cuisine',
-    productName: 'Table à manger',
-    email: '',
-    message: '',
-  });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [familyName, setFamilyName] = useState("");
+  const [productTitle, setProductTitle] = useState("");
+  const [productCategory, setProductCategory] = useState("");
+  const [productPrice, setProductPrice] = useState(0);
+  const [inquiryType, setInquiryType] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const storedProductInfo = localStorage.getItem("productInfo");
+    if (storedProductInfo) {
+      const productInfo = JSON.parse(storedProductInfo);
+      setProductTitle(productInfo.title);
+      setProductPrice(productInfo.price);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true); 
+    try {
+      const formData = {
+        username: name,
+        useremail: email,
+        titleproduct: productTitle,
+        typecommand: inquiryType,
+        priceproduct: productPrice,
+        productcategory: productCategory,
+        message: message
+      };
+      const response = await axios.post(
+        "http://127.0.0.1:8000/submit-form/",
+        formData
+      );
+      console.log("Form submitted successfully:", response.data);
+      localStorage.removeItem("productInfo");
+      toast.success("E-mail envoyé avec succès");
+      setName("");
+      setEmail("");
+      setFamilyName("");
+      setProductTitle("");
+      setProductCategory("");
+      setInquiryType("");
+      setMessage("");
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      toast.error("Envoi échoué");
+    } finally {
+      setLoading(false); 
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-    
+    if (name === "name") setName(value);
+    if (name === "email") setEmail(value);
+    if (name === "familyName") setFamilyName(value);
+    if (name === "productTitle") setProductTitle(value);
+    if (name === "productCategory") setProductCategory(value);
+    if (name === "inquiryType") setInquiryType(value);
+    if (name === "message") setMessage(value);
   };
 
   return (
     <div className="flex flex-col bg-bg h-screen">
-      
-      
-        <form className="w-full flex flex-col gap-2 p-10 " onSubmit={handleSubmit}>
-          <h2 className="text-3xl font-bold mb-6">Contactez-nous !</h2>
-          <div className="flex gap-4 mb-2" >
-            <div className="flex-1">
-              <label className=" text-darkBrown text-sm font-medieum " htmlFor="firstName">
-                Prénom
-              </label>
-              <input
-                type="text"
-                name="firstName"
-                id="firstName"
-                placeholder = "Jane"
-                value={formData.firstName}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
-                required
-              />
-            </div>
-            <div className="flex-1">
-              <label className=" text-darkBrown text-sm font-medieum " htmlFor="lastName">
-                Nom
-              </label>
-              <input
-                type="text"
-                placeholder = "Smitherton"
-                
-                name="lastName"
-                id="lastName"
-                value={formData.lastName}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor "
-                required
-              />
-            </div>
-          </div>
-          <div className="mb-4">
-            <label className="block text-darkBrown text-sm font-medieum  mb-2" htmlFor="inquiryType">
-              Type de demande
-            </label>
-            <select
-              name="inquiryType"
-              id="inquiryType"
-              value={formData.inquiryType}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
-              >
-              <option value="Produit">Produit</option>
-              {/* Ajoutez plus d'options si nécessaire */}
-            </select>
-          </div>
-          <div className='flex gap-4 mb-2'>
+      <form className="w-full flex flex-col gap-2 p-10 " onSubmit={handleSubmit}>
+      <Toaster />
+        <h2 className="text-3xl font-bold mb-6">Contactez-nous !</h2>
+        <div className="flex gap-4 mb-2">
           <div className="flex-1">
-            <label className="block text-darkBrown text-sm font-medieum  mb-2" htmlFor="category">
-              Catégorie
-            </label>
-            <select
-              name="category"
-              id="category"
-              value={formData.category}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
-              >
-              <option value="Cuisine">Cuisine</option>
-              {/* Ajoutez plus d'options si nécessaire */}
-            </select>
-          </div>
-          <div className="flex-1">
-            <label className=" text-darkBrown text-sm font-medieum  mb-2" htmlFor="productName">
-              Nom du produit
-            </label>
+            <label className="text-darkBrown text-sm font-medium" htmlFor="name">Prénom</label>
             <input
               type="text"
-              name="productName"
-              id="productName"
-              value={formData.productName}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
-              />
-          </div>
-          </div>
-          
-          <div className="mb-4">
-            <label className="block text-darkBrown text-sm font-medieum  mb-2" htmlFor="email">
-              Adresse e-mail
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={formData.email}
-              placeholder='email@janesfakedomain.com'
-
+              name="name"
+              id="name"
+              placeholder="Jane"
+              value={name}
               onChange={handleChange}
               className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
               required
             />
           </div>
-          <div className="mb-6">
-            <label className="block text-darkBrown  text-sm font-medieum mb-2" htmlFor="message">
-              Votre message
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              value={formData.message}
+          <div className="flex-1">
+            <label className="text-darkBrown text-sm font-medium" htmlFor="familyName">Nom</label>
+            <input
+              type="text"
+              placeholder="Smitherton"
+              name="familyName"
+              id="familyName"
+              value={familyName}
               onChange={handleChange}
-              placeholder='votre message ici...'
-              className="w-full h-36 px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
+              className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
               required
-            ></textarea>
+            />
           </div>
-          <button
-            type="submit"
-            className="w-full bg-secondary text-white py-2 px-4 rounded-lg hover:bg-red-200"
+        </div>
+        <div className="mb-4">
+          <label className="block text-darkBrown text-sm font-medium mb-2" htmlFor="inquiryType">Type de demande</label>
+          <select
+            name="inquiryType"
+            id="inquiryType"
+            value={inquiryType}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
           >
-            Soumettre
-          </button>
-        </form>
-      </div>
-  
+            <option value="Produit">Produit</option>
+          </select>
+        </div>
+        <div className="flex gap-4 mb-2">
+          <div className="flex-1">
+            <label className="block text-darkBrown text-sm font-medium mb-2" htmlFor="productCategory">Catégorie</label>
+            <select
+              name="productCategory"
+              id="productCategory"
+              value={productCategory}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
+            >
+              <option value="Cuisine">Cuisine</option>
+            </select>
+          </div>
+          <div className="flex-1">
+            <label className="text-darkBrown text-sm font-medium mb-2" htmlFor="productTitle">Nom du produit</label>
+            <input
+              type="text"
+              name="productTitle"
+              id="productTitle"
+              value={productTitle}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
+            />
+          </div>
+        </div>
+        <div className="mb-4">
+          <label className="block text-darkBrown text-sm font-medium mb-2" htmlFor="email">Adresse e-mail</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            placeholder="email@janesfakedomain.com"
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-darkBrown text-sm font-medium mb-2" htmlFor="message">Votre message</label>
+          <textarea
+            name="message"
+            id="message"
+            value={message}
+            onChange={handleChange}
+            placeholder="votre message ici..."
+            className="w-full h-36 px-3 py-2 border border-primary rounded-lg bg-transparent placeholder-placeholderColor"
+            required
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-secondary text-white py-2 px-4 rounded-lg hover:bg-red-200"
+        >
+          {loading ? "Loading..." : "Soumettre"}
+        </button>
+      </form>
+    </div>
   );
 };
 
--- server/main.py
+++ server/main.py
@@ -1,36 +1,34 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel,ValidationError 
+from pydantic import BaseModel, ValidationError
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 from fastapi.middleware.cors import CORSMiddleware
-from google.oauth2 import service_account
-from google.auth.transport.requests import Request
 
 app = FastAPI()
 
-
 origins = [
     "http://localhost",
-    "http://localhost:3000",  
+    "http://localhost:3000",
+    "http://localhost:5173",  # Add your frontend's origin here
 ]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-
 class CommandInfo(BaseModel):
-    username: str   
+    username: str
     useremail: str
     titleproduct: str
     typecommand: str
     priceproduct: float
     productcategory: str
+    message: str
 
 @app.post("/submit-form/")
 async def submit_form(command_info: CommandInfo):
@@ -38,17 +36,16 @@ async def submit_form(command_info: CommandInfo):
         send_email(command_info)
     except ValidationError as e:
         raise HTTPException(status_code=422, detail=str(e))
-
     return {"message": "Form submitted successfully"}
 
 def send_email(command_info: CommandInfo):
     smtp_host = 'smtp.gmail.com'
     smtp_port = 587
     smtp_user = 'adjissifatimaamina@gmail.com'
-    smtp_pass = 'ltds owub ybol pwne'  
+    smtp_pass = 'ltds owub ybol pwne'
     sender_email = smtp_user
-    receiver_email = 'adjissifatimaamina@gmail.com'  
-    
+    receiver_email = 'adjissifatimaamina@gmail.com'
+
     message = MIMEMultipart()
     message['From'] = sender_email
     message['To'] = receiver_email
@@ -62,10 +59,9 @@ def send_email(command_info: CommandInfo):
     Product Info:
     Title: {command_info.titleproduct}
     Command Type : {command_info.typecommand}
-    Price: {
-        command_info.priceproduct}
+    Price: {command_info.priceproduct}
     Category: {command_info.productcategory}
-   
+    Message: {command_info.message}
     """
 
     message.attach(MIMEText(email_body, 'plain'))

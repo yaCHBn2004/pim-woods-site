d666e1d33cd0356fcf292a3ba97829efedcefbaf
whoamif <adjissifatimaamina@gmail.com>
2024-06-20 00:34:49 +0100

    fix : some padding added and styling fixed

--- client/src/pages/Contact/Main.jsx
+++ client/src/pages/Contact/Main.jsx
@@ -5,14 +5,14 @@ const Main = () => {
   return (
     <div className="w-screen min-h-screen flex items-center justify-center">
       <div
-        className="flex-1 min-h-screen flex  "
+        className="flex-1 min-h-screen  flex m-auto  "
         style={{
           backgroundImage: `url('/src/assets/images/contact.jpg')`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
       >
-         <h1 className=' text-6xl pt-44  pl-12 text-bg font-normal w-[70%]'>Timeless Solid <br /> <br /> Crafts</h1>
+         <h1 className=' text-6xl pb-24 m-auto text-bg font-normal w-[70%]'>Timeless Solid <br /> <br /> Crafts</h1>
       </div>
       <div className="flex-1 min-h-screen flex items-center justify-center">
         <Form />

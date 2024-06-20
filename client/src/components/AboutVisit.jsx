f8779e5a1fd94ed402ef4fc183c7faf7a78a1d34
Imed Bousakhria <i_bousakhria@estin.dz>
2024-06-20 23:46:22 +0100

    fix: added scrolling to map to arrow

--- client/src/components/AboutVisit.jsx
+++ client/src/components/AboutVisit.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import GoToFooter from '../utils/GoToFooter'
 import inarrow from "../assets/icons/arrows/in-arrow.svg";
+import { scrollToBottom } from '../hooks/scrollToFooter';
 
 const AboutVisit = () => {
     return (
         <div className="text-secondary flex items-center gap-2 cursor-pointer ">
             <GoToFooter />
-            <img className="" src={inarrow} alt="" />
+            <img className="" onClick={scrollToBottom} src={inarrow} alt="" />
         </div>
         )
 }

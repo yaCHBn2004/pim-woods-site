38e037495e7a5db0f72fb3a15d1a464ec5e47207
Imed Bousakhria <i_bousakhria@estin.dz>
2024-06-21 16:53:36 +0100

    same fix (attempt)

--- client/src/content/designs.js
+++ client/src/content/designs.js
@@ -1,33 +1,32 @@
-// i just make this file to simulate a database of designs and to use it in the app to display the designs in landingpage 
-import high1 from "../assets/images/highlighted/high1.jpg"
-import high2 from "../assets/images/highlighted/high2.jpg"
-import high3 from "../assets/images/highlighted/high3.jpg"
-import high4 from "../assets/images/highlighted/high4.jpg"
+// i just make this file to simulate a database of designs and to use it in the app to display the designs in landingpage
+import high1 from "../assets/images/highlighted/high1.jpg";
+import high2 from "../assets/images/highlighted/high2.jpg";
+import high3 from "../assets/images/highlighted/high3.jpg";
+import high4 from "../assets/images/highlighted/high4.jpg";
 
 export const designs = [
-    {
-      id: 1,
-      title: "Salle à manger",
-      image: {high1},
-      designer: "Hadj Bakli"
-    },
-    {
-      id: 2,
-      title: "Cuisine",
-      image: {high2},
-      designer: "Bousakhria"
-    },
-    {
-      id: 3,
-      title: "Salon",
-      image: {high3},
-      designer: "Adjissi"
-    },
-    {
-      id: 4,
-      title: "Chambre",
-      image: {high4},
-      designer: "Cheurfi Belhadj"
-    }
-  ];
-  
+  {
+    id: 1,
+    title: "Salle à manger",
+    image: high1 ,
+    designer: "Hadj Bakli",
+  },
+  {
+    id: 2,
+    title: "Cuisine",
+    image: high2,
+    designer: "Bousakhria",
+  },
+  {
+    id: 3,
+    title: "Salon",
+    image: high3,
+    designer: "Adjissi",
+  },
+  {
+    id: 4,
+    title: "Chambre",
+    image: high4,
+    designer: "Cheurfi Belhadj",
+  },
+];

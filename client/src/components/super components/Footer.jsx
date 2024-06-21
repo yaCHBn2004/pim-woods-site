9583ccb0b2a1ef5c2a082969a0e8336a074a14b1
Imed Bousakhria <i_bousakhria@estin.dz>
2024-06-21 19:08:20 +0100

    responsive: footer

--- client/src/components/super components/Footer.jsx
+++ client/src/components/super components/Footer.jsx
@@ -8,7 +8,7 @@ import Map from '../Map'
 const Footer = () => {
   return (
     <div className='bg-darkBrown flex flex-col '>
-      <div className=' p-12 md:max-lg:p-6 flex justify-between text-bg'>
+      <div className=' p-12 md:max-lg:p-6 flex justify-between text-bg phone:max-md:flex-col phone:max-md:justify-center phone:max-md:gap-[3.5rem]'>
         <FootLogo />
 
         <div className=' flex flex-col gap-5 mx-[2rem] items-start'>
@@ -17,18 +17,20 @@ const Footer = () => {
           <Socials />
         </div>
 
-        <div className=' flex flex-col gap-5 ml-[3rem] items-start'>
+        <div className=' flex flex-col gap-5 ml-[3rem] phone:max-md:mx-[2rem]  items-start'>
           <h3 className='font-medium'>Contacts</h3>
           <Contacts />
         </div>
 
-        <div><Map /></div>
+        <div>
+          <Map />
+        </div>
 
       </div>
 
-      <span className=' text-center py-4 border-t border-t-light-text text-[0.63rem] font-semibold text-light-text paragraph'>
-      © 2024 All Rights Reserved
-      </span>
+        <span className=' text-center py-4 border-t border-t-light-text text-[0.63rem] font-semibold text-light-text paragraph'>
+          © 2024 All Rights Reserved
+        </span>
       </div>
   )
 }

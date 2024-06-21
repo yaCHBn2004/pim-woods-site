ee07804299bea90712e222a0d6926cf70fa04f6f
Imed Bousakhria <i_bousakhria@estin.dz>
2024-06-21 19:16:10 +0100

    .

--- client/src/pages/Landing page/Main.jsx
+++ client/src/pages/Landing page/Main.jsx
@@ -12,9 +12,9 @@ const Main = () => {
         <div className='mx-auto'>
             <BackToTopButton />
             <Header />
-            {/* <About />
+            <About />
             <Trends />
-            <Contact /> */}
+            <Contact />
         </div>
 
     )

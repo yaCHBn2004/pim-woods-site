78fcb83150c81f820cb95f9c36039dbea8f0b747
Imed Bousakhria <i_bousakhria@estin.dz>
2024-06-21 16:50:38 +0100

    fix: high img objects

--- client/src/content/designs.js
+++ client/src/content/designs.js
@@ -1,27 +1,32 @@
 // i just make this file to simulate a database of designs and to use it in the app to display the designs in landingpage 
+import high1 from "../assets/images/highlighted/high1.jpg"
+import high2 from "../assets/images/highlighted/high2.jpg"
+import high3 from "../assets/images/highlighted/high3.jpg"
+import high4 from "../assets/images/highlighted/high4.jpg"
+
 export const designs = [
     {
       id: 1,
       title: "Salle à manger",
-      image: "./src/assets/images/highlighted/high1.jpg",
+      image: {high1},
       designer: "Hadj Bakli"
     },
     {
       id: 2,
       title: "Cuisine",
-      image: "./src/assets/images/highlighted/high2.jpg",
+      image: {high2},
       designer: "Bousakhria"
     },
     {
       id: 3,
       title: "Salon",
-      image: "./src/assets/images/highlighted/high3.jpg",
+      image: {high3},
       designer: "Adjissi"
     },
     {
       id: 4,
       title: "Chambre",
-      image: "./src/assets/images/highlighted/high4.jpg",
+      image: {high4},
       designer: "Cheurfi Belhadj"
     }
   ];
